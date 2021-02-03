b94a3147fac418f433eb2c9b13a9c9760b483b95
Marvin A. Ruder <ruder.marvin@icloud.com>
2021-02-03 20:41:55 +0100

    Fix file selection

--- de.uhd.ifi.se.decision.management.eclipse/src/de/uhd/ifi/se/decision/management/eclipse/event/ShowGraphInJira.java
+++ de.uhd.ifi.se.decision.management.eclipse/src/de/uhd/ifi/se/decision/management/eclipse/event/ShowGraphInJira.java
@@ -4,33 +4,17 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.FileEditorInput;
 
 public class ShowGraphInJira extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbench workbench = PlatformUI.getWorkbench();
-		IWorkbenchWindow window = 
-		        workbench == null ? null : workbench.getActiveWorkbenchWindow();
-		IWorkbenchPage activePage = 
-		        window == null ? null : window.getActivePage();
-
-		IEditorPart editor = 
-		        activePage == null ? null : activePage.getActiveEditor();
-		IEditorInput input = 
-		        editor == null ? null : editor.getEditorInput();
-		IPath path = input instanceof FileEditorInput 
-		        ? ((FileEditorInput)input).getPath()
-		        : null;
-		if (path != null) {
-			OpenWebbrowser.openWebpage(path);
+		IPath pathOfSelectedFile = CommandHelper.getPathOfSelectedFile(event);
+		if (pathOfSelectedFile == null || !pathOfSelectedFile.toFile().exists()) {
+			return null;
+		}
+		if (pathOfSelectedFile != null) {
+			OpenWebbrowser.openWebpage(pathOfSelectedFile);
 		}
 
 		return null;
