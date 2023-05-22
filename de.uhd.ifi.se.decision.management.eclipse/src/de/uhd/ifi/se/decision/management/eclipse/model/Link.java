7aa6de88380511c6d90c4623c7b1bd1c15f17b44
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2023-05-22 18:42:31 +0200

    Fix broken test

--- de.uhd.ifi.se.decision.management.eclipse/src/de/uhd/ifi/se/decision/management/eclipse/model/Link.java
+++ de.uhd.ifi.se.decision.management.eclipse/src/de/uhd/ifi/se/decision/management/eclipse/model/Link.java
@@ -1,13 +1,18 @@
 package de.uhd.ifi.se.decision.management.eclipse.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import de.uhd.ifi.se.decision.management.eclipse.model.impl.LinkImpl;
+
 /**
  * Interface for links (=edges) in the knowledge graph.
  * 
  * @see KnowledgeGraph
  * @see Node
  */
+@JsonDeserialize(as = LinkImpl.class)
 public interface Link {
-	
+
 	/**
 	 * Retrieves the source element of this link (= edge).
 	 *
@@ -15,7 +20,7 @@ public interface Link {
 	 * @return source element of this link.
 	 */
 	Node getSource();
-	
+
 	/**
 	 * Retrieves the source node of this link (= edge).
 	 *
@@ -23,27 +28,27 @@ public interface Link {
 	 * @return source node of this link.
 	 */
 	Node getSourceNode();
-	
+
 	/**
 	 * Sets the source element of this link (= edge).
 	 * 
 	 * @param source
-	 * 		the source node
+	 *            the source node
 	 */
 	void setSourceNode(Node source);
-	
+
 	/**
 	 * Retrieves the source id of this link (= edge).
 	 *
 	 * @return source id of this link.
 	 */
 	String getSourceId();
-	
+
 	/**
 	 * Sets the source id of this link (= edge).
 	 * 
 	 * @param sourceId
-	 * 		the source id
+	 *            the source id
 	 */
 	void setSourceId(String sourceId);
 
@@ -54,7 +59,7 @@ public interface Link {
 	 * @return destination element of this link.
 	 */
 	Node getTarget();
-	
+
 	/**
 	 * Retrieves the target (= destination) node of this link (= edge).
 	 *
@@ -62,27 +67,27 @@ public interface Link {
 	 * @return destination node of this link.
 	 */
 	Node getTargetNode();
-	
+
 	/**
 	 * Retrieves the target id of this link (= edge).
 	 *
 	 * @return target id of this link.
 	 */
 	String getTargetId();
-	
+
 	/**
 	 * Sets the target id of this link (= edge).
 	 * 
 	 * @param targetId
-	 * 		the target id
+	 *            the target id
 	 */
 	void setTargetId(String targetId);
-	
+
 	/**
 	 * Sets the target (= destination) node of this link (= edge).
 	 * 
 	 * @param target
-	 * 		the target node
+	 *            the target node
 	 */
 	void setTargetNode(Node target);
 }
